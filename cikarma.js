ede137d741137e27d8ef82802dd1c90a5b861417
Eslem Arik <eslemarik20@outlook.com>
2023-01-22 14:00:27 +0300

    Cikarma islemi icin duzenleme yapildi

--- cikarma.js
+++ cikarma.js
@@ -1,3 +1,6 @@
 function cikar() {
-    sonuc.innerHTML = sayi1.value - sayi2.value;
+    if(parseInt(sayi1.value) < parseInt(sayi2.value))
+    alert("Cikarma islemi icin birinci sayinin ikinci sayidan buyuk olmasi gerekiyor!");
+    else
+    sonuc.innerHTML = parseInt (sayi1.value) - parseInt (sayi2.value);
 }
